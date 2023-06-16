787b944d2dc38978190d7cca4223454b30b864c3
ADDIA-Mohcine <addia0moha@gmail.com>
2023-06-16 12:42:58 -0700

    .

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,23 +7,18 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	printf("%d is ", n);
 	if (n > 0)
-	{
-		printf("%d is positive\n".n);
-	}
+		printf("positive\n");
 	else if (n == 0)
-	{
-		printf("%d is zero\n".n);
-	}
+		printf("zero\n");
 	else
-	{
-		printf("%d is negative\n".n);
-	}
-	return (0);
+		printf("negative\n");
+	return 0;
 }
