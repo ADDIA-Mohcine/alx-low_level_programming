8d291824b270f82ca1928cb7d5d5e8fd71bc4d74
ADDIA-Mohcine <addia0moha@gmail.com>
2023-06-16 12:36:09 -0700

    .

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,16 @@ int main(void)
 	printf("%d is ", n);
 
 	if (n > 0)
+	{
 		printf("%d is positive\n");
+	}
 	else if (n == 0)
+	{
 		printf("%d is zero\n");
+	}
 	else
+	{
 		printf("%d is negative\n");
+	}
 	return (0);
 }
