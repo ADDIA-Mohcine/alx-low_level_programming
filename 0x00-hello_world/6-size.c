55b20bc6ba984aa30c540e0d5e9c9e9324ff6149
ADDIA-Mohcine <addia0moha@gmail.com>
2023-06-16 00:51:18 -0700

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,9 @@
 
 int main(void)
 {
-	/* This program prints the size of various types on the computer it is compiled and run on. */
+	/* This program prints the size of various
+	 *  types on the computer it is compiled
+	 *  and run on. */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
