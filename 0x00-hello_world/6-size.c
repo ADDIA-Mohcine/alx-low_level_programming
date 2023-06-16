50b70ee97de3b34ca468d68e5ea06d9320eb8257
ADDIA-Mohcine <addia0moha@gmail.com>
2023-06-16 00:40:43 -0700

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * Return: 0 if exited properly, non-zero otherwise
  */
 
-int main(void)
+int main (void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
