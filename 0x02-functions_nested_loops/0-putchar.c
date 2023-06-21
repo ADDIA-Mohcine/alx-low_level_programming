0ce22b92555fa7ad02f5dcdcf7406e67dfa08682
ADDIA-Mohcine <addia0moha@gmail.com>
2023-06-20 18:47:07 -0700

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
 	 * is written by mbah
 	 * Return: 0
 	 */
-int main(void)
+	int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,5 +16,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	
-	}
+}
